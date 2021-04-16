28f60366f1a7d172cb6ade226768e844c9d4d8ac
Sal Ferrarello <sal@ironcodestudio.com>
2021-04-16 09:48:43 -0400

    Add "Say hi" button that alerts "hello"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 
+function sayHello() {
+  alert('hello');
+}
+
 function App() {
   return (
     <div className="App">
+      <button onClick={sayHello}>Say hi</button>
     </div>
   );
 }
